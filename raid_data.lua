be5ef30db01a58a66419ba1d3d2c47b22ac4363e
Ghezzo <1338300+Ghezzo@users.noreply.github.com>
2024-05-04 07:53:58 +0200

    added awakened raids

--- raid_data.lua
+++ raid_data.lua
@@ -19,4 +19,21 @@ addon.raidData = {
     [2467] = { numEncounters = 2, startFrom =  1 },
     [2468] = { numEncounters = 2, startFrom =  1 },
     [2469] = { numEncounters = 2, startFrom =  1 },
+
+    -- Vaults of the Incarnates Awakened
+    [2703] = { numEncounters = 3, startFrom =  1 },
+    [2705] = { numEncounters = 3, startFrom =  1 },
+    [2706] = { numEncounters = 2, startFrom =  1 },
+    
+    -- Aberrus Awakened
+    [2704] = { numEncounters = 3, startFrom =  1 },
+    [2707] = { numEncounters = 3, startFrom =  1 },
+    [2708] = { numEncounters = 2, startFrom =  1 },
+    [2709] = { numEncounters = 1, startFrom =  1 },
+    
+        -- Amirdrassil Awakened
+    [2710] = { numEncounters = 3, startFrom =  1 },
+    [2711] = { numEncounters = 2, startFrom =  1 },
+    [2712] = { numEncounters = 2, startFrom =  1 },
+    [2713] = { numEncounters = 2, startFrom =  1 },
 }
