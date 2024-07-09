c2df7c6cc66bd4802db8fe06639781f13d61da13
Ghezzo <1338300+Ghezzo@users.noreply.github.com>
2024-07-09 22:52:15 +0200

    update

--- raid_data.lua
+++ raid_data.lua
@@ -31,9 +31,32 @@ addon.raidData = {
     [2708] = { numEncounters = 2, startFrom =  1 },
     [2709] = { numEncounters = 1, startFrom =  1 },
     
-        -- Amirdrassil Awakened
+    -- Amirdrassil Awakened
     [2710] = { numEncounters = 3, startFrom =  1 },
     [2711] = { numEncounters = 2, startFrom =  1 },
     [2712] = { numEncounters = 2, startFrom =  1 },
     [2713] = { numEncounters = 2, startFrom =  1 },
+
+    -- Mogu'shan Vaults Remix
+	[2598] = { numEncounters = 3, startFrom =  1 }, -- Guardians of Mogu'shan
+	[2597] = { numEncounters = 3, startFrom =  1 }, -- The Vault of Mysteries
+ 
+	-- Heart of Fear Remix
+	[2596] = { numEncounters = 3, startFrom =  1 }, -- The Dread Approach
+	[2595] = { numEncounters = 3, startFrom =  1 },-- Nightmare of Shek'zeer
+ 
+	-- Terrace of Endless Spring Remix
+	[2599] = { numEncounters = 4, startFrom =  1 },
+ 
+	-- Throne of Thunder Remix
+	[2594] = { numEncounters = 3, startFrom =  1 }, -- Last Stand of the Zandalari
+	[2593] = { numEncounters = 3, startFrom =  1 }, -- Forgotten Depths
+	[2592] = { numEncounters = 3, startFrom =  1 }, -- Halls of Flesh-Shaping
+	[2591] = { numEncounters = 3, startFrom =  1 }, -- Pinnacle of Storms
+ 
+	-- Siege of Ogrimmar Remix
+	[2590] = { numEncounters = 4, startFrom =  1 }, -- Vale of Eternal Sorrows
+	[2589] = { numEncounters = 4, startFrom =  1 }, -- Gates of Retribution
+	[2588] = { numEncounters = 3, startFrom =  1 }, -- The Underhold
+	[2587] = { numEncounters = 3, startFrom =  1 }, -- Downfall
 }
